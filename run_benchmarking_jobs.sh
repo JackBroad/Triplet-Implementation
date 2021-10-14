b0ef5946eff719af88e82386c0e069e80ebd16bd
Jack Broad <pmxjb9@login001.int.augusta.nottingham.ac.uk>
2021-10-14 14:53:58 +0100

    Uncommented line to actually run the job in run_benchmarking_jobs.sh

--- run_benchmarking_jobs.sh
+++ run_benchmarking_jobs.sh
@@ -7,5 +7,5 @@ maxProcessors=$1
 # Loop over no. of procs and run job for each N_proc
 for ((i=1; i<=$maxProcessors; i++)); do
   sed -ie 's/[0-9]\+ /'$i' /' myJob.sh; # Update myJob.sh
-  #sbatch --partition=defq --nodes=1 --ntasks-per-node=$i --mem=40g --time=1:00:00 --output=$i-data.txt myJob.sh
+  sbatch --partition=defq --nodes=1 --ntasks-per-node=$i --mem=40g --time=1:00:00 --output=$i-data.txt myJob.sh
 done
